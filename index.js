4e309ce23b6ee682314322c14a774a22a2a221db
Jackrob <gr0bs@outlook.com>
2020-10-11 16:22:54 +0200

    Ajouts page login

--- index.js
+++ index.js
@@ -3,12 +3,29 @@ const APP = EXPRESS()
 const HTTP = require('http').createServer(APP);
 const IO = require('socket.io')(HTTP);
 
+// const MONGOOSE = require('mongoose')
+
+// // Bodyparser pour convertir les données en json
+// const BODYPARSER = require('body-parser')
+
+// APP.use(BODYPARSER.json())
+
+// //require les données json
+// require('./models/user')(APP)
+
+//Routes
 APP.get('/', (req, res) => {
   res.sendFile(__dirname + '/routes/tchat/tchat.html')
 });
 
+// condition si connecté ou pas pour redirigé ici
+APP.get('/login', (req, res) => {
+  res.sendFile(__dirname + '/routes/login/login.html')
+})
+
 APP.use(EXPRESS.static(__dirname + '/routes/tchat'))
 
+//Socket io connection et interaction
 IO.on('connect', (socket) => {
   console.log('a user connected')
   socket.on('new-user', data => {
@@ -24,6 +41,8 @@ IO.on('connect', (socket) => {
   })
 });
 
+
+//server
 HTTP.listen(3000, () => {
   console.log('listening on *:3000')
 })
---
+++ models/user.js
@@ -0,0 +1,6 @@
+let mongoose = require('mongoose')
+let UserSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+})
+module.exports = mongoose.model('UserSchema', UserSchema)
